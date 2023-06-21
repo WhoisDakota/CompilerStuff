cdfaac7752c7d1d2c755fabf03670607f25e6207
Dakota <99370868+WhoisDakota@users.noreply.github.com>
2023-06-21 15:38:01 -0700

    code cleaned up, no symbol table

--- parser.cpp
+++ parser.cpp
@@ -1,112 +1,130 @@
 //
-// Created by dakota on 6/17/23.
+// Dakota Burke
 //
 
 #include "parser.h"
 #include <iostream>
 #include <istream>
 #include <string>
+#include <cctype>
+#include <stdlib.h>
 
 #include "lexer.h"
+#include "parser.h"
 #include "inputbuf.h"
 
-/*
- * need one function for each non-terminal of grammar to handle
- * parsing of such non-terminal
- * ie:
- * parse_X()
- * */
-
-Parser::Parser(){
-    //tmp.lexeme = "";
-    //tmp.token_type = ERROR;
-}
-
-void Parser::parse_Program(LexicalAnalyzer lexer) {
+int Parser::parse_Program(void) {
    Token t =  lexer.GetToken();
-   //check first set (First(Program): {ID})
-   if(t.token_type == ID){
-       lexer.UngetToken(t);
-       parse_global_vars(lexer);
-       parse_scope(lexer);
-       printf("Program->global_vars.scope\n");
-   }
-   //no need to check follow set as while global_vars has epsilon in first set, scope does not.
-   else{
-       syntax_error();
+    int returnVal;
 
+   //check first set (First(Program): {ID})
+   while(t.token_type != END_OF_FILE) {
+       if (t.token_type == ID) {
+           Token t = lexer.GetToken();
+           if(t.token_type == LBRACE){
+               tempTType = lexer.UngetToken(t);
+               returnVal = parse_scope();
+               std::cout << "\n Parsed program -> epsilon scope \n";
+           }
+           else if (t.token_type == COMMA){
+               tempTType = lexer.UngetToken(t);
+               returnVal = parse_global_vars();
+               returnVal = parse_scope();
+               std::cout << "\n Parsed program -> global_vars scope";
+           }
+           else if(t.token_type == END_OF_FILE){
+           return(0);
+            }
+           else {
+               syntax_error();
+           }
+       }
+       t = lexer.GetToken();
    }
+   return(0);
 }
 
-void Parser::parse_global_vars(LexicalAnalyzer lexer) {
+int Parser::parse_global_vars(void) {
     Token t = lexer.GetToken();
+    int returnVal;
     //check first set (First(global_vars): {ID, epsilon} )
-    if(t.token_type == ID){
-        lexer.UngetToken(t);
-        parse_var_list(lexer);
+    if (t.token_type == ID) {
         t = lexer.GetToken();
-        if(t.token_type == SEMICOLON){
-            printf("global_vars->var_list.SEMICOLON\n");
+        if (t.token_type == COMMA) {
+            tempTType = lexer.UngetToken(t);
+            returnVal = parse_var_list();
+            t = lexer.GetToken();
+            if (t.token_type == SEMICOLON) {
+                std::cout << "global_vars->var_list.SEMICOLON\n";
+            }
+            else {
+                syntax_error();
+            }
         }
-        else{
-            printf("Syntax Error\n");
+        else if (t.token_type == LBRACE) {
+            tempTType = lexer.UngetToken(t);
+            returnVal = parse_scope();
+            std::cout << "global_vars -> epsilon -> scope";
+        }
+        else {
+            syntax_error();
         }
     }
-    // epsilon exists in first set, so check follow set see if t exists in follow(global_vars): {$, RBRACE}
-    else if(t.token_type == RBRACE){
+    else{
+        syntax_error();
+    }
+        // epsilon exists in first set, so check follow set see if t exists in follow(global_vars): {$, RBRACE}
+    /*else if (t.token_type == RBRACE) {
         lexer.UngetToken(t);
         printf("global_vars->epsilon\n");
+        return (0);
     }
-    else if(t.token_type == END_OF_FILE){
-        printf("global_vars->EOF\n");
-    }
-    else
-        syntax_error();
-        //printf("Syntax Error");
+    */
+    return(0);
 }
 
-void Parser::parse_var_list(LexicalAnalyzer lexer) {
+
+int Parser::parse_var_list(void) {
     Token t = lexer.GetToken();
+    int returnVal;
     //check first set (First(var_list): {ID} )
 
     if(t.token_type == ID){
-        Token tmpToken = lexer.GetToken();
-        if (tmpToken.token_type == COMMA){
-            parse_var_list(lexer);
-            printf("var_list->ID COMMA var_list\n");
+        Token t = lexer.GetToken();
+        if (t.token_type == COMMA){
+            returnVal = parse_var_list();
+            std::cout << "var_list->ID COMMA var_list\n";
         }
-        else if (lexer.IsKeyword(tmpToken.lexeme) && tmpToken.token_type != ID){
-            lexer.UngetToken(tmpToken);
-            printf("var_list->ID\n");
+        else if (lexer.IsKeyword(t.lexeme) && t.token_type != ID){
+            tempTType = lexer.UngetToken(t);
+            std::cout << "var_list->ID\n";
         }
         else{
             syntax_error();
         }
     }
-    //epsilon does NOT exist in first(var_list), therefore no need to check follow set.
     else{
         syntax_error();
-        //printf("Syntax Error");
     }
-
+    return(0);
 }
 
-void Parser::parse_scope(LexicalAnalyzer lexer) {
+int Parser::parse_scope(void) {
     Token t = lexer.GetToken();
+    int returnVal;
     //First(scope): {ID}
     if(t.token_type == ID){
-        Token tmp1 = lexer.GetToken();
-        if(tmp1.token_type == LBRACE){
-            lexer.UngetToken(tmp1);
-            parse_public_vars(lexer);
-            parse_private_vars(lexer);
-            parse_stmt_list(lexer);
-            tmp1 = lexer.GetToken();
-            if(tmp1.token_type == RBRACE){
+        Token t = lexer.GetToken();
+        if(t.token_type == LBRACE){
+            tempTType = lexer.UngetToken(t);
+            returnVal = parse_public_vars();
+            returnVal = parse_private_vars();
+            returnVal = parse_stmt_list();
+            t = lexer.GetToken();
+            if(t.token_type == RBRACE){
                 printf("Scope->ID LBRACE public_vars private_vars stmt_list RBRACE\n");
             }
             else{
-                //lexer.UngetToken(tmp1);
                 syntax_error();
             }
         }
@@ -116,51 +134,53 @@ void Parser::parse_scope(LexicalAnalyzer lexer) {
     else{
         syntax_error();
     }
-
+return(0);
 }
 
-void Parser::parse_public_vars(LexicalAnalyzer lexer) {
+int Parser::parse_public_vars(void) {
     Token t = lexer.GetToken();
+    int returnVal;
     //First(public_vars): {PUBLIC, epsilon}
     if(t.token_type == PUBLIC){
-        Token tmp = lexer.GetToken();
-        if(tmp.token_type == COLON){
-            parse_var_list(lexer);
-            Token tmp2 = lexer.GetToken();
-            if(tmp2.token_type == SEMICOLON){
+        Token t = lexer.GetToken();
+        if(t.token_type == COLON){
+            returnVal = parse_var_list();
+            t = lexer.GetToken();
+            if(t.token_type == SEMICOLON){
                 printf("public_vars-> PUBLIC COLON var_list SEMICOLON");
             }
             else{
-                //lexer.UngetToken(tmp2);
                 syntax_error();
             }
         }
         else{
-            //lexer.UngetToken(tmp);
             syntax_error();
         }
     }
     //Epsilon exists in first(public_vars), check Follow(public_vars)
-    else if(t.token_type == RBRACE){
-        lexer.UngetToken(t);
-        printf("public_vars->epsilon\n");
+    else if(t.token_type == PRIVATE){
+        tempTType = lexer.UngetToken(t);
+        returnVal = parse_private_vars();
+        std::cout << "\npublic_vars->epsilon -> private_vars\n";
     }
     else{
         syntax_error();
     }
+    return(0);
 }
 
-void Parser::parse_private_vars(LexicalAnalyzer lexer) {
+int Parser::parse_private_vars(void) {
     Token t = lexer.GetToken();
+    int returnVal;
     //First(private_vars): {PRIVATE, epsilon}
     if(t.token_type == PRIVATE){
         t = lexer.GetToken();
         if(t.token_type == COLON){
-            lexer.UngetToken(t);
-            parse_var_list(lexer);
+            tempTType = lexer.UngetToken(t);
+            returnVal = parse_var_list();
             t = lexer.GetToken();
             if(t.token_type == SEMICOLON){
-                printf("private_vars->PRIVATE COLON var_list SEMICOLON");
+                std::cout << "\nprivate_vars->PRIVATE COLON var_list SEMICOLON\n";
             }
             else{
                 syntax_error();
@@ -171,81 +191,81 @@ void Parser::parse_private_vars(LexicalAnalyzer lexer) {
         }
     }
     //Epsilon exists in first(private_vars), check if t exists in follow(private_vars): {RBRACE}
-    else if(t.token_type == RBRACE){
-        lexer.UngetToken(t);
-        printf("private_vars->epsilon");
+    else if(t.token_type == ID){
+        tempTType = lexer.UngetToken(t);
+        returnVal = parse_stmt_list();
+        std::cout <<"\nprivate_vars->epsilon->stmt_list\n";
     }
     else{
         syntax_error();
     }
-
+    return(0);
 }
 
-void Parser::parse_stmt_list(LexicalAnalyzer lexer) {
+int Parser::parse_stmt_list(void) {
     Token t = lexer.GetToken();
+    int returnVal;
     //First(stmt_list): {RBRACE}
-    if(t.token_type == ID){
-        lexer.UngetToken(t);
-        parse_stmt(lexer);
+    if(t.token_type == ID) {
+        tempTType = lexer.UngetToken(t);
+        returnVal = parse_stmt();
         t = lexer.GetToken();
-        //check first rule stmt_list -> stmt
-        if(t.token_type == ID){
-            lexer.UngetToken(t);
-            parse_stmt_list(lexer);
-            printf("stmt_list->stmt stmt_list");
+        if(t.token_type == ID) {
+            tempTType = lexer.UngetToken(t);
+            std::cout << "\n stmt_list-> stmt stmt_list\n";
+            returnVal = parse_stmt_list();
         }
-        else if(t.token_type != ID){
-            printf("stmt_list->stmt");
+        else if(t.token_type == RBRACE){
+            tempTType = lexer.UngetToken(t);
+            std::cout << "\n stmt_list-> stmt\n";
         }
     }
-    //epsilon NOT in first set, so no follow set to check
-
     else
     {
         syntax_error();
     }
+    return(0);
 }
 
-void Parser::parse_stmt(LexicalAnalyzer lexer) {
+int Parser::parse_stmt(void) {
     Token t = lexer.GetToken();
+    int returnVal;
     //First(stmt): {ID}
     if(t.token_type == ID){
-        Token t2 = lexer.GetToken();
-        if(t2.token_type == EQUAL){
-            Token t3 = lexer.GetToken();
-            if(t3.token_type == ID){
-                Token t4 = lexer.GetToken();
-                if(t4.token_type == SEMICOLON){
-                    printf("stmt->ID EQUAL ID SEMICOLON");
+        t = lexer.GetToken();
+        if(t.token_type == EQUAL){
+             t = lexer.GetToken();
+            if(t.token_type == ID){
+                 t = lexer.GetToken();
+                if(t.token_type == SEMICOLON){
+                    std::cout << "\nstmt->ID EQUAL ID SEMICOLON\n";
                 }
                 else{
-                    lexer.UngetToken(t4);
                     syntax_error();
                 }
             }
             else{
-                lexer.UngetToken(t3);
                 syntax_error();
             }
         }
-        else if (t2.token_type == ID){      //stmt->scope
-            lexer.UngetToken(t2);
-            parse_scope(lexer);
-            printf("stmt->scope");
+        else if (t.token_type == LBRACE){      //stmt->scope
+            tempTType = lexer.UngetToken(t);
+            returnVal = parse_scope();
+            std::cout << "\nstmt->scope\n";
         }
         else{
-            lexer.UngetToken(t2);
             syntax_error();
         }
     }
     else{
         syntax_error();
     }
+    return(0);
 }
 
 
 
-void Parser::syntax_error() {
+int Parser::syntax_error(void) {
     std::cout << "Syntax Error\n";
     exit(1);
 
@@ -254,14 +274,8 @@ void Parser::syntax_error() {
 
 int main()
 {
-    Parser parser;
-    LexicalAnalyzer lexer;
-    Token t = lexer.GetToken();
+   int returnValue;
+    Parser* parse = new Parser();
+    returnValue = parse->parse_Program();
 
-
-
-    while(t.token_type != END_OF_FILE){
-        lexer.UngetToken(t);
-        parser.parse_Program(lexer);
-    }//TODO might encounter EOF issues here without checking for token. Maybe
 }
